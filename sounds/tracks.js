8b2e15115b58d64993f314982f26296f2a688f8f
Danny Adams <dantheado@gmail.com>
2020-09-14 16:32:58 +0100

    Added all images

--- sounds/tracks.js
+++ sounds/tracks.js
@@ -3,26 +3,26 @@ const tracks = [
     id: '1',
     url: require('./zen_dong.mp3'),
     title: 'Meditation Complete',
-    artwork: require('../images/elijah-hiett-wW0BUXTTUmU-unsplash.jpg'),
+    artwork: require('../images/36.jpg'),
   },
   {
     id: '3',
     url: require('./ding-dong-dung.mp3'),
     title: 'Meditation Complete',
-    artwork: require('../images/elijah-hiett-wW0BUXTTUmU-unsplash.jpg'),
+    artwork: require('../images/36.jpg'),
   },
   {
     id: '4',
     url: require('./23539__loofa__thai-gong1.mp3'),
     title: 'Meditation Complete',
 
-    artwork: require('../images/elijah-hiett-wW0BUXTTUmU-unsplash.jpg'),
+    artwork: require('../images/36.jpg'),
   },
   {
     id: '5',
     url: require('./23540__loofa__thai-gong2.mp3'),
     title: 'Meditation Complete',
-    artwork: require('../images/elijah-hiett-wW0BUXTTUmU-unsplash.jpg'),
+    artwork: require('../images/36.jpg'),
   },
 ];
 
